47bcfc9634f68b14abafa6b1f161a0dd52ef6c2c
Wang Jiaming <1584824430@qq.com>
2017-05-13 20:50:42 +0800

    swiper test

--- meituan/src/scene/PageOne.js
+++ meituan/src/scene/PageOne.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import {View, Text, StyleSheet, StatusBar,Dimensions, Platform, PixelRatio,
+	ListView,Image, TouchableOpacity, ScrollView, RefreshControl} from 'react-native';
+	import { Actions } from 'react-native-router-flux';
+	import ScrollableTabView, {ScrollableTabBar } from 'react-native-scrollable-tab-view';
+	import Swiper from 'react-native-swiper';
 
-export default class PageOne extends Component {
-  render() {
-    return (
-      <View style={{margin: 128}}>
-        <Text onPress={Actions.pageTwo}>这里是页面一
-        </Text>
-        
-      </View>
-    )
-  }
-}
+	export default class PageOne extends Component {
+		render() {
+			return (
+
+				<Swiper  showsButtons={true}>
+				<View style={{
+					backgroundColor:'red',
+					height:500,
+
+				}}>
+				<Text style={{
+					color:'white',
+					fontSize:40,
+					textAlign:'center',
+				}}>PPPPPPPPPage one</Text>
+				</View>
+				<View style={{
+					backgroundColor:'blue',
+					height:500,
+				}}>
+				<Text style={{
+					color:'white',
+					fontSize:40,
+					textAlign:'center',
+				}}>PPPPPPPPPage two</Text>
+				</View>
+				<View style={{
+					backgroundColor:'green',
+					height:500,
+				}}>
+				<Text style={{
+					color:'white',
+					fontSize:40,
+					textAlign:'center',
+				}} >PPPPPPPPPage three</Text>
+				</View>
+				</Swiper>
+
+				);
+
+		}
+
+	}
+
+
