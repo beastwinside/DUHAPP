3eef62404f09a91bbc6788934dcf08d2e9df1879
Wang Jiaming <1584824430@qq.com>
2017-05-16 17:59:05 +0800

    json page test

--- meituan/src/scene/PageTest.js
+++ meituan/src/scene/PageTest.js
@@ -3,6 +3,20 @@ import {
 	View, Text, StyleSheet, StatusBar, ListView,
 	Image, TouchableOpacity, ScrollView, RefreshControl} from 'react-native';
 	import { Actions } from 'react-native-router-flux';
+	import Orderdata from '../data/orderdata.json';
+
+
+	var imgurl=new Array();
+	imgurl.push(
+		require('../img/order/xiaofuqi@2x.png'),
+		require('../img/order/laofuqi@2x.png'),
+		require('../img/order/dafuqi@2x.png'),
+		require('../img/order/zhongfuqi@2x.png'),
+		require('../img/order/jufuqi@2x.png'),
+		require('../img/order/shaofuqi@2x.png'),
+		require('../img/order/duofuqi@2x.png'),
+		require('../img/order/xiaoxiaofuqi@2x.png')
+		);
 
 	export default class PageTest extends Component {
 		constructor(props){
@@ -10,75 +24,150 @@ import {
 			const ds=new ListView.DataSource({
 				rowHasChanged:(r1,r2)=>r1!=r2});
 			this.state={
-				dataSource:ds.cloneWithRows(
-					[
-					{
-						shopname:'小夫妻',
-						orderstatus:'订单完成',
-						shopimageurl:require('../img/order/xiaofuqi@2x.png'),
-						ordername:'香菇滑稽饭',
-						ordernum:'1',
-						orderprice:'10',
-					},
-					{
-						shopname:'老夫妻',
-						orderstatus:'订单完成',
-						shopimageurl:require('../img/order/laofuqi@2x.png'),
-						ordername:'黄焖鸡米饭',
-						ordernum:'1',
-						orderprice:'10',
-					},{
-						shopname:'大夫妻',
-						orderstatus:'订单完成',
-						shopimageurl:require('../img/order/dafuqi@2x.png'),
-						ordername:'蛋炒饭',
-						ordernum:'1',
-						orderprice:'10',
-					},{
-						shopname:'中夫妻',
-						orderstatus:'订单取消',
-						shopimageurl:require('../img/order/zhongfuqi@2x.png'),
-						ordername:'热狗套餐',
-						ordernum:'1',
-						orderprice:'12',
-					},
-					{
-						shopname:'巨夫妻',
-						orderstatus:'订单完成',
-						shopimageurl:require('../img/order/jufuqi@2x.png'),
-						ordername:'汉堡',
-						ordernum:'3',
-						orderprice:'10',
-					},
-					{
-						shopname:'少夫妻',
-						orderstatus:'订单完成',
-						shopimageurl:require('../img/order/shaofuqi@2x.png'),
-						ordername:'童子 鸡',
-						ordernum:'2',
-						orderprice:'11',
-					},
-					{
-						shopname:'多夫妻',
-						orderstatus:'订单完成',
-						shopimageurl:require('../img/order/duofuqi@2x.png'),
-						ordername:'米线',
-						ordernum:'1',
-						orderprice:'10',
-					},
-					{
-						shopname:'小小夫妻',
-						orderstatus:'订单完成',
-						shopimageurl:require('../img/order/xiaoxiaofuqi@2x.png'),
-						ordername:'馒头',
-						ordernum:'15',
-						orderprice:'1',
-					},
-					])
+				dataSource:ds.cloneWithRows(Orderdata.orderinfo)
 			};
 
 
 
+		}
+
+
+		_renderRow(rowData: string,sectionID: number, rowID: number)
+		{	var imgSource=imgurl[rowID]; 
+			return(
+				<View style={{
+					height:150,
+					backgroundColor:'#212121',
+					borderBottomWidth:1.5,
+					borderColor:'white',
+				}}>
+				<View style={{
+					flex:0.8,
+					backgroundColor:'#2b2e2e',
+					flexDirection:'row',
+				}}>
+
+				<TouchableOpacity activeOpacity={0.6}>
+				<Image 
+				style={{
+					marginLeft:10,
+					marginTop:5,
+					height:40,
+					width:40,
+					borderRadius:25,
+				}}
+				source={imgSource}/>
+
+				</TouchableOpacity>
+				<Text
+				style={{
+					color:'#E6E5E5',
+					fontSize:20,
+					marginTop:10,
+					marginLeft:10,
+				}}
+				>{rowData.shopname}</Text>
+
+				<Text
+				style={{
+					color:'#BCBCBC',
+					fontSize:15,
+					marginTop:10,
+					position:'absolute',
+					right:5,
+
+				}}
+				>{rowData.orderstatus}</Text>
+
+
+				</View>
+
+
+				<View style={{
+					flex:1,
+					backgroundColor:'#2b2e2e',
+
+
+
+				}}>
+				<TouchableOpacity activeOpacity={0.6}>
+				<Text
+				style={{
+					color:'#FFFFFF',
+					fontSize:18,
+					marginTop:25,
+					marginLeft:60,
+					fontWeight:'bold',
+				}}
+				>{rowData.ordername}</Text>
+
+				<View	style={{
+
+					position:'absolute',
+					right:0,
+					flexDirection:'column',
+
+				}}>
+
+				<Text
+				style={{
+
+					color:'#FFFFFF',
+					fontSize:10,
+					textAlign:'right',
+					marginTop:15,
+					marginRight:15,
+				}}
+				>{'x'+rowData.ordernum}</Text>
+				<Text
+				style={{
+
+					color:'#FFFFFF',
+					fontSize:12,
+					marginRight:5,
+
+				}}
+				>{'共'+rowData.ordernum+'件商品 '+'实付'
+				+rowData.ordernum*rowData.orderprice+'元'}
+				</Text>
+
+
+				</View>
+				</TouchableOpacity>
+				</View>
+
+
+
+
+				<View style={{
+					flex:0.8,
+					backgroundColor:'#2b2e2e',
+				}}>
+				<View	style={{
+					marginTop:7,
+					position:'absolute',
+					right:10,
+					borderColor:'#d4442a',
+					borderWidth:1,
+					height:30,
+					width:80,
+					backgroundColor:'#d4442a',
+
+
+				}}>
+				<TouchableOpacity activeOpacity={0.6}>
+				<Text style={{
+
+					fontSize:17,
+					color:'#FFFFFF',
+					textAlign:'center',
+				}}>再来一单</Text>
+				</TouchableOpacity>
+				</View>
+				</View>
+
+				</View>
+				);
 		}
 
 
@@ -92,141 +181,7 @@ import {
 				<ListView 
 
 				dataSource={this.state.dataSource}
-				renderRow={(rowData)=>
-					<View style={{
-						height:150,
-						backgroundColor:'#212121',
-						borderBottomWidth:1.5,
-						borderColor:'white',
-					}}>
-					<View style={{
-						flex:0.8,
-						backgroundColor:'#2b2e2e',
-						flexDirection:'row',
-					}}>
-
-					<TouchableOpacity activeOpacity={0.6}>
-					<Image 
-					style={{
-						marginLeft:10,
-						marginTop:5,
-						height:40,
-						width:40,
-						borderRadius:25,
-					}}
-					source={rowData.shopimageurl}/>
-
-					</TouchableOpacity>
-					<Text
-					style={{
-						color:'#E6E5E5',
-						fontSize:20,
-						marginTop:10,
-						marginLeft:10,
-					}}
-					>{rowData.shopname}</Text>
-
-					<Text
-					style={{
-						color:'#BCBCBC',
-						fontSize:15,
-						marginTop:10,
-						position:'absolute',
-						right:5,
-
-					}}
-					>{rowData.orderstatus}</Text>
-
-
-					</View>
-
-
-					<View style={{
-						flex:1,
-						backgroundColor:'#2b2e2e',
-					
-						
-						
-					}}>
-					<TouchableOpacity activeOpacity={0.6}>
-					<Text
-					style={{
-						color:'#FFFFFF',
-						fontSize:18,
-						marginTop:25,
-						marginLeft:60,
-						fontWeight:'bold',
-					}}
-					>{rowData.ordername}</Text>
-
-					<View	style={{
-						
-						position:'absolute',
-						right:0,
-						flexDirection:'column',
-						
-					}}>
-
-					<Text
-					style={{
-
-						color:'#FFFFFF',
-						fontSize:10,
-						textAlign:'right',
-						marginTop:15,
-						marginRight:15,
-					}}
-					>{'x'+rowData.ordernum}</Text>
-					<Text
-					style={{
-						
-						color:'#FFFFFF',
-						fontSize:12,
-						marginRight:5,
-						
-					}}
-					>{'共'+rowData.ordernum+'件商品 '+'实付'
-					+rowData.ordernum*rowData.orderprice+'元'}
-					</Text>
-
-
-					</View>
-					</TouchableOpacity>
-					</View>
-
-
-
-
-					<View style={{
-						flex:0.8,
-						backgroundColor:'#2b2e2e',
-					}}>
-					<View	style={{
-						marginTop:7,
-						position:'absolute',
-						right:10,
-						borderColor:'#d4442a',
-						borderWidth:1,
-						height:30,
-						width:80,
-						backgroundColor:'#d4442a',
-
-						
-					}}>
-					<TouchableOpacity activeOpacity={0.6}>
-					<Text style={{
-
-						fontSize:17,
-						color:'#FFFFFF',
-						textAlign:'center',
-					}}>再来一单</Text>
-					</TouchableOpacity>
-					</View>
-					</View>
-
-					</View>
-
-				}
+				renderRow={this._renderRow}
 
 				/>
 
